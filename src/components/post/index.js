dc708c528d496b0e08fc9ee733abceb687312088
AlexanderKhapchenko <alexanderfox1337@gmail.com>
2021-05-25 05:09:14 +0300

    Add likes and author caption

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -3,13 +3,25 @@ import PropTypes from 'prop-types';
 import Header from './header';
 import Image from './image';
 import Actions from './actions';
+import Footer from './footer';
 
 
 export default function Post({ content }) {
+
+	const commentInput = useRef(null);
+
+	const handleFocus = () => commentInput.current.focus();
 	return (
 		<div className="rounded col-span-4 border bg-white border-gray-primary mb-16">
 			<Header username={content.username} />
 			<Image src={content.imageSrc} caption={content.caption} />
+			<Actions
+				docId={content.docId}
+				totalLikes={content.likes.length}
+				likedPhoto={content.userLikedPhoto}
+				handleFocus={handleFocus}
+			/>
+			<Footer caption={content.caption} username={content.username}/>
 		</div>
 	);
 }
