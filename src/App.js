251f4136dac425e2d9ca450e6c449cf43a1d2259
AlexanderKhapchenko <alexanderfox1337@gmail.com>
2021-05-25 06:13:00 +0300

    added redirection when user login and not login

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import UserContext from './context/user';
 import useAuthListener from './hooks/use-auth-listener';
+import ProtectedRoute from './heplers/protected-route';
+import IsUserLoggedIn from './heplers/is-user-logged-in';
 
 const Login = lazy(() => import ('./pages/login'));
 const SignUp = lazy(() => import ('./pages/sign-up'));
@@ -17,9 +19,15 @@ function App() {
 			<Router>
 				<Suspense fallback={<p>Loading...</p>}>
 					<Switch>
-						<Route path={ROUTES.LOGIN} component={Login} />
-						<Route path={ROUTES.SIGN_UP} component={SignUp} />
-						<Route path={ROUTES.DASHBOARD} component={Dashboard} />
+						<IsUserLoggedIn user={user} loggedInPath={ROUTES.DASHBOARD} path={ROUTES.LOGIN}>
+							<Login />
+						</IsUserLoggedIn>
+						<IsUserLoggedIn user={user} loggedInPath={ROUTES.DASHBOARD} path={ROUTES.SIGN_UP}>
+							<SignUp />
+						</IsUserLoggedIn>
+						<ProtectedRoute user={user} path={ROUTES.DASHBOARD} exact>
+							<Dashboard />
+						</ProtectedRoute>
 						<Route component={NotFound} />
 					</Switch>
 				</Suspense>
